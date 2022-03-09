6c6ef83ae07b6337e9a3f6ff5bb669e9036ad3c0
Alina Safina <linasafina16@gmail.com>
2022-03-09 14:06:36 +0300

    Fix console.log in task 1 and task 2

--- homework_1/script.js
+++ homework_1/script.js
@@ -6,23 +6,23 @@ const num2 = +prompt('Please, enter the second number');
 if (Number.isNaN(num1) || Number.isNaN(num2)) {
   console.log('Некорректный ввод!');
 } else {
-  const result = num1.toString(num2);
-  console.log(result);
+  console.log(num1.toString(num2));
 }
 
 // Task 2
 
-const number1 = +prompt('Please, enter the first number');
+const ERROR_MESSAGE = 'Некорректный ввод!';
+const PROMPT_MESSAGE = 'Please, enter the number';
+const number1 = +prompt(PROMPT_MESSAGE);
 
 if (Number.isNaN(number1)) {
-  console.log('Некорректный ввод!');
+  console.log(ERROR_MESSAGE);
 } else {
-  const number2 = +prompt('Please, enter the second number');
+  const number2 = +prompt(PROMPT_MESSAGE);
+
   if (Number.isNaN(number2)) {
-    console.log('Некорректный ввод!');
+    console.log(ERROR_MESSAGE);
   } else {
-    const sum = number1 + number2;
-    const quot = number1 / number2;
-    console.log(`Ответ: ${sum}, ${quot}`);
+    console.log(`Ответ: ${number1 + number2}, ${number1 / number2}`);
   }
 }
