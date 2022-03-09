2e0ad30ced2159ec88284fa6d48f5d2307223b35
Alina Safina <linasafina16@gmail.com>
2022-03-09 13:23:02 +0300

    Finish task 2

--- homework_1/script.js
+++ homework_1/script.js
@@ -1,11 +1,28 @@
 // Task 1
 
-const num1 = +prompt("Please, enter the first number");
-const num2 = +prompt("Please, enter the second number");
+const num1 = +prompt('Please, enter the first number');
+const num2 = +prompt('Please, enter the second number');
 
-if (isNaN(num1) || isNaN(num2)) {
-  console.log("Некорректный ввод!");
+if (Number.isNaN(num1) || Number.isNaN(num2)) {
+  console.log('Некорректный ввод!');
 } else {
   const result = num1.toString(num2);
   console.log(result);
+}
+
+// Task 2
+
+const number1 = +prompt('Please, enter the first number');
+
+if (Number.isNaN(number1)) {
+  console.log('Некорректный ввод!');
+} else {
+  const number2 = +prompt('Please, enter the second number');
+  if (Number.isNaN(number2)) {
+    console.log('Некорректный ввод!');
+  } else {
+    const sum = number1 + number2;
+    const quot = number1 / number2;
+    console.log(`Ответ: ${sum}, ${quot}`);
+  }
 }
