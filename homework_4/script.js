61038c66fe9abbc2cc2fd15ceda19454e4413ab9
Alina Safina <linasafina16@gmail.com>
2022-03-16 22:29:23 +0300

    Finish the 2nd task without the ability to copy the function into a variable

--- homework_4/script.js
+++ homework_4/script.js
@@ -1,20 +1,74 @@
+// Task 1
+
 function concatStrings(str, separator) {
   if (typeof str !== 'string') {
     return str || 'Вы ничего не ввели';
   }
 
-  // str = separator ? str + separator : str;
-
   function innerFunc(str2) {
     if (typeof str2 !== 'string') {
       return str;
     }
+
     str = separator ? str + separator : str;
+
     return concatStrings(str + str2, separator);
   }
 
   return innerFunc;
 }
 
-console.log(concatStrings('some-value')('333')(123n));
-console.log(concatStrings('some-value')(2));
+// Task 2
+
+class Calculator {
+  constructor(num1, num2, ...arg) {
+    if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+      throw new Error('Only numbers are accepted!');
+    }
+
+    if (arg.length !== 0) {
+      throw new Error('Please, enter only TWO numbers!');
+    }
+
+    this.num1 = num1;
+    this.num2 = num2;
+  }
+  setX(num) {
+    if (!num || typeof num !== 'number') {
+      throw new Error('Please, enter ONE number');
+    }
+
+    this.num1 = num;
+  }
+  setY(num) {
+    if (!num || typeof num !== 'number') {
+      throw new Error('Please, enter ONE number');
+    }
+
+    this.num2 = num;
+  }
+  logSum() {
+    console.log(this.num1 + this.num2);
+  }
+  logMul() {
+    console.log(this.num1 * this.num2);
+  }
+  logSub() {
+    console.log(this.num1 - this.num2);
+  }
+  logDiv() {
+    if (this.num2 === 0) {
+      throw new Error('Cannot divide by 0');
+    }
+
+    console.log(this.num1 + this.num2);
+  }
+}
+
+const calculator = new Calculator(12, 3);
+calculator.setX(13);
+
+const mulpiply = calculator.logMul;
+console.log(mulpiply);
+const calc = { num1: 10, num2: 2 };
+mulpiply.call(calc);
