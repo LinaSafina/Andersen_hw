c03d5137144160a2eb4b41c0ddc0043a888497d9
Alina Safina <lina.zumba@mail.ru>
2022-04-01 20:00:07 +0300

    Fix +/- and rounding

--- homework_7/calculator.js
+++ homework_7/calculator.js
@@ -1,8 +1,17 @@
 const input = document.querySelector('#input');
+const buttons = document.querySelectorAll('button');
 
-document.querySelectorAll('button').forEach((button) => {
+input.textContent = '';
+
+buttons.forEach((button) => {
   button.addEventListener('click', function () {
     calculate(this.textContent);
+  });
+});
+
+buttons.forEach((button) => {
+  button.addEventListener('click', function () {
+    checkLength();
   });
 });
 
@@ -10,14 +19,40 @@ function calculate(value) {
   const isError =
     input.textContent === '' &&
     (value === '*' || value === '/' || value === '.');
+
   if (isError) {
-    alert('Please, enter a number first');
+    alert('Please, enter a number first!');
+
     return;
   }
 
   switch (value) {
     case '=':
-      input.textContent = eval(input.textContent).toFixed(8);
+      try {
+        let result = eval(input.textContent);
+
+        checkLength(result);
+
+        if (result === Infinity) {
+          alert('Cannot divide on 0!');
+          return;
+        }
+
+        const isNeedRounding =
+          !Number.isInteger(result) &&
+          result.toString().length - result.toString().indexOf('.') > 9;
+
+        if (isNeedRounding) {
+          input.textContent = result.toFixed(8);
+
+          return;
+        }
+
+        input.textContent = result;
+      } catch (e) {
+        alert(e.message);
+      }
+
       break;
 
     case 'C':
@@ -25,21 +60,66 @@ function calculate(value) {
       break;
 
     case '>':
-      input.textContent = input.textContent.substring(0, -1);
+      input.textContent = input.textContent.slice(0, -1);
       break;
 
     case '+/-':
-      if (
+      const isNegative =
         input.textContent[input.textContent.length - 1] === ')' &&
-        input.textContent.slice(0, 2) === '-('
-      ) {
+        input.textContent.slice(0, 2) === '-(';
+      const isNotOneNumber =
+        input.textContent.includes('+', 1) ||
+        input.textContent.includes('-', 1) ||
+        input.textContent.includes('/') ||
+        input.textContent.includes('*');
+
+      if (isNegative && isNotOneNumber) {
         input.textContent = input.textContent.slice(2, -1);
-      } else {
-        input.textContent = '-(' + input.textContent + ')';
+
+        return;
       }
+
+      if (isNotOneNumber) {
+        input.textContent = '-(' + input.textContent + ')';
+
+        return;
+      }
+
+      if (!isNotOneNumber && input.textContent[0] === '-') {
+        input.textContent = input.textContent.slice(1);
+
+        return;
+      }
+
+      if (!isNotOneNumber) {
+        input.textContent = '-' + input.textContent;
+
+        return;
+      }
+
       break;
 
     default:
       input.textContent += value;
+  }
+}
+
+function checkLength() {
+  if (input.textContent.length <= 12) {
+    input.style.fontSize = '2.5em';
+
+    return;
+  }
+
+  if (input.textContent.length > 12 && input.textContent.length < 18) {
+    input.style.fontSize = '1.7em';
+
+    return;
+  }
+
+  if (input.textContent.length > 18) {
+    input.style.fontSize = '1.3em';
+
+    return;
   }
 }
